516bebc165dfc4a6fa0991bd9e33c3558346eb5c
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-02-05 17:05:18 +0100

    add switch tab function for tabs

--- src/js/modules/components/tabs.js
+++ src/js/modules/components/tabs.js
@@ -14,3 +14,30 @@ tabButtons.forEach((tab, index) => {
         tabPanels[index].setAttribute('hidden', '');
     }
 });
+
+tabsContainer.addEventListener('click', evt => {
+    // If we don't click on a link, we don't want JS to do anything
+    // Look for closest link when click, if not a link = null
+    const clickedTab = evt.target.closest('a');
+    // Guard clause, if no tab clicked (null), escape and don't do any more
+    if (!clickedTab) return;
+    // Prevent default behaviour of link, stop link jumping
+    evt.preventDefault;
+    // Helper Function for switching tab panels
+    switchTab(clickedTab);
+    //
+});
+
+const switchTab = function (newTab) {
+    // When click on a link, get href value (#xxxx)
+    const activePanelId = newTab.getAttribute('href');
+    // Make clicked link the active panel based on the id (#xxxx)
+    const activePanel = tabsContainer.querySelector(activePanelId);
+    // Loop through each of the panels, set to hidden
+    tabPanels.forEach(panel => {
+        panel.setAttribute('hidden', true);
+    });
+    // Set the active panel as not hidden
+    activePanel.removeAttribute('hidden');
+    console.log(activePanelId);
+};
