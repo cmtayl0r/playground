bb2e77df8cd6fe1d32cea3ce8374fb69931635b7
Chris Taylor <134958907+cmtayl0r@users.noreply.github.com>
2024-05-10 20:40:47 +0200

    add dropdown component trapFocus methods

--- src/js/modules/components/dropdown.js
+++ src/js/modules/components/dropdown.js
@@ -3,14 +3,17 @@
 // --------------------------------------------------------
 class Dropdown {
     constructor(trigger, dropdownData) {
+        // The trigger element that will open the dropdown
         this.dropdownTrigger = trigger;
+        // Data to populate the dropdown
         this.dropdownData = dropdownData;
-        this.dropdown = null; // Initialize the dropdown element holder
-        this.bindEvents();
+        // set to null initially because the dropdown does not exist yet
+        this.dropdown = null;
+        this.initDropdown();
     }
 
     // Initialize the dropdown
-    bindEvents() {
+    initDropdown() {
         // 01 - On trigger click, toggle the dropdown
         this.dropdownTrigger.addEventListener('click', event => {
             // Prevent the event from bubbling up to the document
@@ -19,20 +22,17 @@ class Dropdown {
             event.preventDefault();
             // Toggle the dropdown
             this.toggleDropdown();
+            if (this.dropdown.style.display === 'block') {
+                // If the dropdown is visible, trap focus
+                this.trapFocus();
+            }
         });
         // Add a click event listener to the document to hide the dropdown when clicked outside
-
         document.addEventListener(
             'click',
             this.handleOutsideClick.bind(this),
             false,
         );
-
-        // document.addEventListener(
-        //     'keydown',
-        //     this.trapFocus(event).bind(this),
-        //     false,
-        // );
     }
 
     // Create the dropdown element
@@ -46,6 +46,7 @@ class Dropdown {
         dropdownList.classList.add('dropdown__list');
         dropdown.appendChild(dropdownList);
 
+        // alternate method of injection of HTML markup
         // dropdown.innerHTML = `
         //     <ul class="dropdown__list">
         //         ${this.dropdownData.map(link => `<li><a href="${link.url}">${link.text}</a></li>`).join('')}
@@ -97,12 +98,16 @@ class Dropdown {
         }
     }
 
-    // Handle keyboard events
-    trapFocus(event) {
-        // if the dropdown is not visible, do nothing
-        if (this.dropdown.style.display !== 'block') return;
+    // Trap focus within the dropdown
+    trapFocus() {
+        this.dropdown.addEventListener('keydown', event => {
+            // Call handleKeydown method
+            this.handleKeydown(event);
+        });
+    }
 
-        // Get the first and last focusable elements in the dropdown
+    // Handle keydown events
+    handleKeydown(event) {
         const focusableElements = this.dropdown.querySelectorAll(
             'a[href], button:not([disabled]), input, select, textarea, [tabindex]:not([tabindex="-1"])',
         );
@@ -110,29 +115,30 @@ class Dropdown {
         const lastFocusableElement =
             focusableElements[focusableElements.length - 1];
 
-        // Handle keyboard events
         switch (event.key) {
-            case 'Tab':
-                if (event.shiftKey) {
-                    // if shift + tab, and the first focusable element is focused, move focus to the last focusable element
-                    if (document.activeElement === firstFocusableElement) {
-                        event.preventDefault();
-                        lastFocusableElement.focus();
-                    }
-                } else {
-                    // if tab, and the last focusable element is focused, move focus to the first focusable element
-                    if (document.activeElement === lastFocusableElement) {
-                        event.preventDefault();
-                        firstFocusableElement.focus();
-                    }
-                }
-                break;
             case 'Escape':
-                // if escape, hide the dropdown and set aria-expanded to false
                 this.dropdown.style.display = 'none';
                 this.dropdownTrigger.setAttribute('aria-expanded', 'false');
+                this.dropdownTrigger.focus();
                 break;
-            default:
+            case 'Tab':
+                if (
+                    // If the dropdown is open and the shift key is pressed
+                    event.shiftKey &&
+                    document.activeElement === firstFocusableElement
+                ) {
+                    // If the first focusable element is focused, move focus to the last focusable element
+                    event.preventDefault();
+                    lastFocusableElement.focus();
+                } else if (
+                    // If the dropdown is open and the shift key is not pressed
+                    !event.shiftKey &&
+                    document.activeElement === lastFocusableElement
+                ) {
+                    // If the last focusable element is focused, move focus to the first focusable element
+                    event.preventDefault();
+                    firstFocusableElement.focus();
+                }
                 break;
         }
     }
