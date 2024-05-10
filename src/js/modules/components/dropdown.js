fc791640ba581445e138faad98b7904992fd3e23
Chris Taylor <134958907+cmtayl0r@users.noreply.github.com>
2024-05-10 14:28:49 +0200

    add dropdown component starter files

---
+++ src/js/modules/components/dropdown.js
@@ -0,0 +1,30 @@
+// --------------------------------------------------------
+// Dropdown Component
+// --------------------------------------------------------
+
+const dummyLinks = [
+    {
+        text: 'Link 1',
+        url: '#',
+    },
+    {
+        text: 'Link 2',
+        url: '#',
+    },
+    {
+        text: 'Link 3',
+        url: '#',
+    },
+    {
+        text: 'Link 4',
+        url: '#',
+    },
+    {
+        text: 'Link 5',
+        url: '#',
+    },
+];
+
+export default class Dropdown {
+    constructor() {}
+}
