202c29a7ccae66566e2465f37c65d6989c2bc8c5
Chris Taylor <134958907+cmtayl0r@users.noreply.github.com>
2024-05-13 20:47:45 +0200

    add new form validation refactoring

--- src/js/pages/forms/form-validation.js
+++ src/js/pages/forms/form-validation.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('myForm');
 const inputs = form.querySelectorAll('input, textarea, select');
 
+// TODO: Add validation for groups of checkboxes
+
 document.addEventListener('DOMContentLoaded', () => {
     // Function: Show error message
     function showError(input, message) {
@@ -21,9 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
         formGroup.classList.remove('error');
     }
 
-    // Real-time validation
+    // Real-time validation of inputs on blur
     inputs.forEach(input => {
+        // Check for real-time validation of inputs
         input.addEventListener('blur', () => {
+            // Using the browser's built-in validation
             if (!input.validity.valid) {
                 showError(input, input.validationMessage);
             } else {
@@ -32,24 +36,28 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Validation on form submit
+    // Validation of inputs on form submit
     form.addEventListener('submit', event => {
+        // Check if form is valid, so we can prevent the form from submitting
         let isFormValid = true;
+        // Loop through each input and check if it's valid
         inputs.forEach(input => {
             if (!input.validity.valid) {
+                // show error message inline with input
                 showError(input, input.validationMessage);
+                // Set form as invalid
                 isFormValid = false;
             } else {
+                // clear any error messages
                 clearError(input);
             }
         });
+        // If form is not valid, prevent it from submitting
         if (!isFormValid) {
             event.preventDefault();
-        }
-
-        if (isFormValid) {
             alert('👍 Form is valid');
         } else {
+            // Form is valid, submit it
             alert('❌ Form is NOT valid');
         }
     });
